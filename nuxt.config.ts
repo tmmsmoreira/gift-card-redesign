448c7026d37098bda4fbfa359895e8fda70fd68a
Tiago Moreira <tiago.moreira@dhigroupinc.com>
2024-09-07 23:22:16 +0100

    Project creation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  ssr: false,
+  css: ['~/node_modules/@nordhealth/css/lib/nord.css'],
+
+  vite: {
+    vue: {
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    }
+  },
+
+  compatibilityDate: '2024-09-07',
 });
